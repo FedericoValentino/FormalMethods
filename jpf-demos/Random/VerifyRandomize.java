5697630a38171947660660890783de60db6b9131
FedericoValentino <10679810@polimi.it>
2025-04-30 11:40:54 +0200

    wooo

--- jpf-demos/Random/VerifyRandomize.java
+++ jpf-demos/Random/VerifyRandomize.java
@@ -2,11 +2,39 @@ import gov.nasa.jpf.vm.Verify;
 
 public class VerifyRandomize {
     public static void main(String[] args) {
-        Boolean random = Verify.getBoolean();
-        if (random) {
-            System.out.println("if");
+        Boolean random1 = Verify.getBoolean();
+        Boolean random2 = Verify.getBoolean();
+
+        FlipFlop ff1= new FlipFlop();
+        FlipFlop ff2= new FlipFlop();
+
+        if (random1) {
+            System.out.println("1-set");
+            ff1.set();
         } else {
-            System.out.println("else");
+            System.out.println("1-reset");
+            ff1.reset();
+        }
+        if (random2) {
+            System.out.println("2-set");
+            ff2.set();
+        } else {
+            System.out.println("2-reset");
+            ff2.reset();
+        }
+        if(!ff1.value && !ff2.value){
+            System.out.println("oh oh");
+            int x=1/0;
+        }
+    }
+    static class FlipFlop{
+        public boolean value;
+
+        public void set(){
+            value=true;
+        }
+        public void reset(){
+            value=false;
         }
     }
 }
